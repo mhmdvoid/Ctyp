3920e04bf982136c4ae71460809b68365f4ce417
Mohammed Alghumuy <62178353+mhmdvoid@users.noreply.github.com>
2023-11-02 11:54:51 +0300

    Update Poo.py

--- Poo.py
+++ Poo.py
@@ -1,4 +1,59 @@
-gboolean	 as_store_load_path		(AsStore	*store,
+import ctypes
+from ctypes.util import find_library
+
+class AppstreamError(Exception):
+    pass
+
+class AppstreamComponent:
+    def __init__(self, component):
+        self.component = component
+
+    def get_id(self):
+        return ctypes.string_at(appstream.as_component_get_id(self.component)).decode('utf-8')
+
+    def get_icon(self):
+        return ctypes.string_at(appstream.as_component_get_icon(self.component)).decode('utf-8')
+
+    def get_release(self):
+        return ctypes.string_at(appstream.as_component_get_version(self.component)).decode('utf-8')
+
+try:
+    # Load the appstream-glib library
+    appstream = ctypes.CDLL(find_library('appstream-glib'))
+
+    # Initialize the library
+    appstream.as_init()
+
+    # Create a pool
+    pool = appstream.as_pool_new()
+
+    # Load AppStream data from a file
+    xml_file = "path/to/appstream.xml"
+    error = ctypes.POINTER(ctypes.c_void_p)()
+    if not appstream.as_pool_load_file(pool, xml_file, None, ctypes.byref(error)):
+        error_msg = ctypes.string_at(error.contents).decode('utf-8')
+        raise AppstreamError(f"Failed to load AppStream data: {error_msg}")
+
+    # Get the list of components
+    components = appstream.as_pool_get_components(pool)
+
+    for iter in iter(components):
+        component = AppstreamComponent(iter.contents)
+
+        # Access information
+        app_id = component.get_id()
+        icon = component.get_icon()
+        release = component.get_release()
+
+        print(f"App ID: {app_id}")
+        print(f"Icon: {icon}")
+        print(f"Release: {release}")
+
+finally:
+    # Cleanup and release resources
+    appstream.as_pool_free(pool)
+    appstream.as_cleanup()
+
 						 const gchar	*path,
 						 GCancellable	*cancellable,
 						 GError		**error);
